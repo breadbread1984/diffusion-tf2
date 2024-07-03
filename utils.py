a91327ae85c83cb5ad833f5a39e9aa468bf0c57c
breadbread1984 <breadbread1984@163.com>
2024-07-03 10:47:40 +0800

    update

---
+++ utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import numpy as np
+import tensorflow as tf
+
+def make_beta_schedule(schedule, n_timestep, linear_start = 1e-4, linear_end = 2e-2, cosine_s = 8e-3):
+  if schedule == "linear":
+    betas = tf.cast(tf.linspace(linear_start ** 0.5, linear_end ** 0.5, n_timestep), dtype = tf.float64) ** 2
+  elif schedule == "cosine":
+    timesteps = tf.range(n_timestep + 1, dtype = tf.float64) / n_timestep + consine_s
+    alphas = timesteps / (1 + cosine_s) * np.pi / 2
+    alphas = tf.math.cos(alphas) ** 2
+    alphas = alphas / alphas[0]
+    betas = 1 - alphas[1:] / alphas[:-1]
+    betas = tf.clip_by_value(betas, 0, 0.999)
+  elif schedule == "sqrt_linear":
+    betas = tf.cast(tf.linspace(linear_start, linear_end, n_timestep), dtype = tf.float64)
+  elif schedule == "sqrt":
+    betas = tf.cast(tf.linspace(linear_start, linear_end, n_timestep), dtype = tf.float64) ** 0.5
+  else:
+    raise NotImplementedError("unknown schedule!")
+  return betas
