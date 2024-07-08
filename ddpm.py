0fba8f687de5c7e22ff0c807e75b2110a0b2d26b
breadbread1984 <breadbread1984@163.com>
2024-07-08 15:50:51 +0800

    update

--- ddpm.py
+++ ddpm.py
@@ -76,38 +76,49 @@ class DDPMInfer(tf.keras.Model):
     linear_end = kwargs.get('linear_end', 2e-2)
     cosine_s = kwargs.get('cosine_s', 8e-3)
     parameterization = kwargs.get('parameterization', 'eps') # eps or x0
+    v_posterior = kwargs.get('v_posterior', 0.)
     super(DDPMInfer, self).__init__()
     self.input_shape = input_shape
     self.timesteps = timesteps
     self.parameterization = parameterization
+    self.v_posterior = v_posterior
     self.model = DiffusionWrapper(input_shape, unet_config, condition_key)
     # scheduler
     self.betas = make_beta_schedule(beta_schedule, timesteps, linear_start = linear_start, linear_end = linear_end, cosine_s = cosine_s) # betas.shape = (timesteps)
     self.alphas = 1. - self.betas # alpha_t
     self.alphas_cumprod = tf.math.cumprod(self.alphas, axis = 0) # bar{alpha}_t
-    self.alphas_cumprod_prev = tf.concat([tf.ones([1,], dtype = tf.float32), self.alphas_cumprod[:-1]], axis = 0)
+    self.alphas_cumprod_prev = tf.concat([tf.ones([1,], dtype = tf.float32), self.alphas_cumprod[:-1]], axis = 0) # bar{alpha}_{t-1}
     self.sqrt_alphas_cumprod = tf.math.sqrt(self.alphas_cumprod) # sqrt(bar{alpha}_t)
-    self.sqrt_one_minus_alphas_cumprod = tf.math.sqrt(1. - self.alphas_cumprod) # sqrt(1 - bar{alpha})
-    self.log_one_minus_alphas_cumprod = tf.math.log(1. - self.alphas_cumprod)
-    self.sqrt_recip_alphas_cumprod = tf.math.sqrt(1. / self.alphas_cumprod)
-    self.sqrt_recipm1_alphas_cumprod = tf.math.sqrt(1. / self.alphas_cumprod - 1)
+    self.sqrt_one_minus_alphas_cumprod = tf.math.sqrt(1. - self.alphas_cumprod) # sqrt(1 - bar{alpha}_t)
+    self.log_one_minus_alphas_cumprod = tf.math.log(1. - self.alphas_cumprod) # log(1 - bar{alpha}_t)
+    self.sqrt_recip_alphas_cumprod = tf.math.sqrt(1. / self.alphas_cumprod) # sqrt(1 / bar{alpha}_t)
+    self.sqrt_recipm1_alphas_cumprod = tf.math.sqrt(1. / self.alphas_cumprod - 1) # sqrt(1 / bar{alpha} - 1)
+    self.posterior_variance = (1 - self.v_posterior) * self.betas * (1. - self.alphas_cumprod_prev) / (1. - self.alphas_cumprod) + self.v_posterior * self.betas
+    self.posterior_log_variance_clipped = tf.math.log(tf.maximum(self.posterior_variance, 1e-20))
+    self.posterior_mean_coef1 = self.betas * tf.math.sqrt(self.alphas_cumprod_prev) / (1. - self.alphas_cumprod) # beta_t * sqrt(bar{alpha}_{t-1}) / (1 - bar{alpha}_t)
+    self.posterior_mean_coef2 = (1. - self.alphas_cumprod_prev) * tf.math.sqrt(self.alphas) / (1. - self.alphas_cumprod) # (1 - bar{alpha}_{t-1}) * sqrt(alpha_t) / (1 - bar{alpha}_t)
   def p_sample(self, x, t, clip_denoised = True, repeat_noise = False):
     # backward process
     model_out = self.model(x, t)
     if self.parameterization == 'eps':
       noise = model_out
+      # x_recon = 1/sqrt(bar{alpha}) * eps - sqrt(1 - bar{alpha})/sqrt(bar{alpha}) * noise
       x_recon = extract_into_tensor(self.sqrt_recip_alphas_cumprod, t, x) * x - extract_into_tensor(self.sqrt_recipm1_alphas_cumprod, t, x) * noise
     elif self.parameterization == 'x0':
       x_recon = model_out
     x_recon = tf.clip_by_value(x_recon, -1., 1.) if clip_denoised else x_recon
+    posterior_mean = extract_into_tensor(self.posterior_mean_coef1, t, x) * x_recon + \
+                     extract_into_tensor(self.posterior_mean_coef2, t, x) * x
+    posterior_log_variance = extract_into_tensor(self.posterior_log_variance_clipped, t, x)
+    noise = tf.stack([tf.random.uniform(shape = self.input_shape, dtype = tf.float32)] * tf.shape(x)[0], axis = 0)
+    nonzero_mask = tf.cond(tf.equal(t,0), true_fn = lambda: tf.zeros_like(x) , false_fn = lambda:tf.ones_like(x)) # zero for t == 0 ones for t != 0
+    return posterior_mean + nonzero_mask * tf.math.exp(0.5 * posterior_log_variance) * noise
   def call(self, inputs):
     x_t = tf.random.uniform(shape = self.input_shape, dtype = tf.float32)
     for t in range(self.timesteps)[::-1]:
-      eps_t = self.model(x_t, t)
-      noise = tf.random.uniform(shape = self.input_shape, dtype = tf.float32)
-      x_tm1 = 1 / tf.math.sqrt(self.alphas[t]) * (x_t - self.betas[t] / self.sqrt_one_minus_alphas_cumprod[t] * eps_t) + \
-              self.betas[t] * (1. - self.alphas_cumprod[t-1]) / (1. - self.alphas_cumprod[t]) * noise
-    return x_tm1
+      x_tm1 = self.p_sample(x_t, t)
+      x_t = x_tm1
+    return x_t
 
 if __name__ == "__main__":
   #tf.keras.backend.set_floatx('float64')
